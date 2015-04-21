dfb71348eeca647086cc3ad520bf4c7422f533f5
Andrew <andrew.dale.wylie@gmail.com>
2015-04-21 17:25:26 -0400

    Added entry point for workflow scheduling.

---
+++ src/mapred/org/apache/hadoop/mapred/workflow/WorkflowSubmitter.java
@@ -0,0 +1,13 @@
+package org.apache.hadoop.mapred.workflow;
+
+/**
+ * Submit a workflow for deadline/budget constrained scheduling.
+ */
+public class WorkflowSubmitter {
+
+	// Entry point for execution of workflow scheduling.
+	public static void main(String[] args) {
+		System.out.println("Test.");
+
+	}
+}
