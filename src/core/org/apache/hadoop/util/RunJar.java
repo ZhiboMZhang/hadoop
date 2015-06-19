2c3a9b6a3c698104135d25269fc08fc5d41d66a1
Andrew <andrew.dale.wylie@gmail.com>
2015-06-19 17:18:54 -0400

    Allow jars to be run w/ arguments specified in the jar manifest.
    Also cleaned up warnings.

--- src/core/org/apache/hadoop/util/RunJar.java
+++ src/core/org/apache/hadoop/util/RunJar.java
@@ -18,12 +18,23 @@
 
 package org.apache.hadoop.util;
 
-import java.util.jar.*;
-import java.lang.reflect.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileUtil;
@@ -35,9 +46,9 @@ public class RunJar {
   public static void unJar(File jarFile, File toDir) throws IOException {
     JarFile jar = new JarFile(jarFile);
     try {
-      Enumeration entries = jar.entries();
+      Enumeration<JarEntry> entries = jar.entries();
       while (entries.hasMoreElements()) {
-        JarEntry entry = (JarEntry)entries.nextElement();
+        JarEntry entry = entries.nextElement();
         if (!entry.isDirectory()) {
           InputStream in = jar.getInputStream(entry);
           try {
@@ -71,45 +82,61 @@ public class RunJar {
   /** Run a Hadoop job jar.  If the main class is not in the jar's manifest,
    * then it must be provided on the command line. */
   public static void main(String[] args) throws Throwable {
-    String usage = "RunJar jarFile [mainClass] args...";
+    String usage = "RunJar jarFile [mainClass [args...]]";
 
+    // Jar file name is required.
     if (args.length < 1) {
       System.err.println(usage);
       System.exit(-1);
     }
 
-    int firstArg = 0;
-    String fileName = args[firstArg++];
+    int currentArg = 0;
+
+    String fileName = args[currentArg++];
     File file = new File(fileName);
     if (!file.exists() || !file.isFile()) {
       System.err.println("Not a valid JAR: " + file.getCanonicalPath());
       System.exit(-1);
     }
+
+    // Try to get any properties set in the manifest.
     String mainClassName = null;
+    String arguments = null;
+    String inputDirectory = null;
+    String outputDirectory = null;
 
     JarFile jarFile;
     try {
       jarFile = new JarFile(fileName);
-    } catch(IOException io) {
-      throw new IOException("Error opening job jar: " + fileName)
-        .initCause(io);
-    }
+      Manifest manifest = jarFile.getManifest();
 
-    Manifest manifest = jarFile.getManifest();
-    if (manifest != null) {
-      mainClassName = manifest.getMainAttributes().getValue("Main-Class");
+      if (manifest != null) {
+        // Class-Name attribute may be set even if others aren't.
+        try {
+          mainClassName = manifest.getMainAttributes().getValue("Main-Class");
+        } catch (IllegalArgumentException ia) {}
+        try {
+          arguments = manifest.getMainAttributes().getValue("Arguments");
+          inputDirectory = manifest.getMainAttributes().getValue("Input-Directory");
+          outputDirectory = manifest.getMainAttributes().getValue("Output-Directory");
+        } catch (IllegalArgumentException ia) {}
+      }
+    } catch (IOException io) {
+      throw new IOException("Error opening job jar: " + fileName).initCause(io);
     }
     jarFile.close();
 
+    // Check if a main class-name was supplied.
     if (mainClassName == null) {
       if (args.length < 2) {
         System.err.println(usage);
         System.exit(-1);
       }
-      mainClassName = args[firstArg++];
+      mainClassName = args[currentArg++];
     }
     mainClassName = mainClassName.replaceAll("/", ".");
 
+    // Unjar files to build up a valid classpath.
     File tmpDir = new File(new Configuration().get("hadoop.tmp.dir"));
     tmpDir.mkdirs();
     if (!tmpDir.isDirectory()) { 
@@ -136,31 +163,47 @@ public class RunJar {
     unJar(file, workDir);
     
     ArrayList<URL> classPath = new ArrayList<URL>();
-    classPath.add(new File(workDir+"/").toURL());
-    classPath.add(file.toURL());
-    classPath.add(new File(workDir, "classes/").toURL());
+    classPath.add(new File(workDir + "/").toURI().toURL());
+    classPath.add(file.toURI().toURL());
+    classPath.add(new File(workDir, "classes/").toURI().toURL());
     File[] libs = new File(workDir, "lib").listFiles();
     if (libs != null) {
       for (int i = 0; i < libs.length; i++) {
-        classPath.add(libs[i].toURL());
+        classPath.add(libs[i].toURI().toURL());
       }
     }
     
-    ClassLoader loader =
-      new URLClassLoader(classPath.toArray(new URL[0]));
+    ClassLoader loader = new URLClassLoader(classPath.toArray(new URL[0]));
 
+    // Set up and execute the jar file.
     Thread.currentThread().setContextClassLoader(loader);
     Class<?> mainClass = Class.forName(mainClassName, true, loader);
     Method main = mainClass.getMethod("main", new Class[] {
       Array.newInstance(String.class, 0).getClass()
     });
-    String[] newArgs = Arrays.asList(args)
-      .subList(firstArg, args.length).toArray(new String[0]);
+    // Main-class and classpath are properly set, just consider args + io.
+    List<String> newArgsList = new ArrayList<String>();
+    String[] newArgs = null;
+    if (args.length < 3 && inputDirectory != null && outputDirectory != null) {
+      newArgsList.addAll(Arrays.asList(inputDirectory, outputDirectory));
+      if (!arguments.equals("")) {
+        newArgsList.addAll(Arrays.asList(arguments.split(" ")));
+      }
+    } else {
+      newArgsList.addAll(Arrays.asList(args).subList(currentArg, args.length));
+      // Remove the main class from the arguments if it's also in the manifest.
+      if (mainClassName != null && newArgsList.contains(mainClassName)) {
+        newArgsList.remove(mainClassName);
+      }
+    }
+    newArgs = newArgsList.toArray(new String[0]);
+
     try {
+      System.out.println("Running " + mainClass.getCanonicalName()
+          + " with args: " + Arrays.toString(newArgs));
       main.invoke(null, new Object[] { newArgs });
     } catch (InvocationTargetException e) {
       throw e.getTargetException();
     }
   }
-  
 }
